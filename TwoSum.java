a4c5c4df7d64c964f6e4141831cc08504fc37a77
Eric Chin <ericychin94@gmail.com>
2021-09-13 21:40:03 -0700

    Added comments to code

--- TwoSum.java
+++ TwoSum.java
@@ -29,36 +29,56 @@ import java.util.*;
 
 public class TwoSum {
 
+    /*
+     * Time Complexity: O(N) where N is the length of our nums array.
+     * 
+     * Space Complexity: O(N) where N is the length of our nums array.
+     */
+
     public static int[] twoSum(int[] nums, int target) {
+        // This map tracks the indices of numbers in our array.
         HashMap<Integer, Integer> map = new HashMap<>();
 
+        // We iterate through our nums array.
         for (int i = 0; i < nums.length; i++) {
+            // Find the difference between the target and the current num.
             int difference = target - nums[i];
+            // If our map contains the difference (Meaning we came across this difference).
             if (map.containsKey(difference)) {
+                // We found our answer so return the index of the difference and the current
+                // index.
                 return new int[] { map.get(difference), i };
-            } else {
-                map.put(nums[i], i);
+            } else { // If our map does not contain the difference.
+                map.put(nums[i], i); // Add the current number and its index in the array.
             }
         }
 
+        // Return null if there is no pair of indices that add to target.
         return null;
     }
 
     public static void main(String[] args) {
+        // Sample arrays.
         int[] sampleOne = new int[] { 2, 7, 11, 15 };
         int[] sampleTwo = new int[] { 3, 2, 4 };
         int[] sampleThree = new int[] { 3, 3 };
 
+        // Sample targets.
         int targetOne = 9;
         int targetTwo = 6;
         int targetThree = 6;
 
+        // Results.
         int[] resultOne = twoSum(sampleOne, targetOne);
         int[] resultTwo = twoSum(sampleTwo, targetTwo);
         int[] resultThree = twoSum(sampleThree, targetThree);
 
-        System.out.println("S1: The two indices that sum to " + targetOne + " are: " + Arrays.toString(resultOne));
-        System.out.println("S2: The two indices that sum to " + targetTwo + " are: " + Arrays.toString(resultTwo));
-        System.out.println("S3: The two indices that sum to " + targetThree + " are: " + Arrays.toString(resultThree));
+        // Print results.
+        System.out
+                .println("Sample 1: The two indices that sum to " + targetOne + " are: " + Arrays.toString(resultOne));
+        System.out
+                .println("Sample 2: The two indices that sum to " + targetTwo + " are: " + Arrays.toString(resultTwo));
+        System.out.println(
+                "Sample 3: The two indices that sum to " + targetThree + " are: " + Arrays.toString(resultThree));
     }
 }
