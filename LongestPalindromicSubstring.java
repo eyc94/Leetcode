812b84e1c0087e614f85029dc88589aa580564ae
Eric Chin <ericychin94@gmail.com>
2021-09-14 00:19:32 -0700

    Added template

---
+++ LongestPalindromicSubstring.java
@@ -0,0 +1,28 @@
+/*
+--- Longest Palindromic Substring ---
+
+Given a string "s", return the longest palindromic substring in "s".
+
+--- EXAMPLES ---
+
+- EX 1 -
+Input:      s = "babad"
+Output:     "bab"
+Note:       "aba" is also a valid answer
+
+- EX 2 -
+Input:      s = "cbbd"
+Output:     "bb"
+
+- EX 3 -
+Input:      s = "a"
+Output:     "a"
+
+- EX 4 -
+Input:      s = "ac"
+Output:     "a"
+*/
+
+public class LongestPalindromicSubstring {
+
+}
