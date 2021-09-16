a2b0aa6a84a4f9f26e23503b9c1f03a9ebf335fe
Eric Chin <ericychin94@gmail.com>
2021-09-15 21:07:48 -0700

    Added comments

--- ThreeSum.java
+++ ThreeSum.java
@@ -25,30 +25,54 @@ import java.util.*;
 
 public class ThreeSum {
 
+    /*
+     * Time Complexity: O(N^2)
+     * 
+     * Space Complexity: O(1) if we don't count the final returned list of lists.
+     */
+
     public static List<List<Integer>> threeSum(int[] nums) {
+        // Sort the array. O(N log N) time.
         Arrays.sort(nums);
+        // List of lists to hold triplets that sum to zero.
         List<List<Integer>> result = new ArrayList<>();
 
+        // Iterate through our nums array until the 3rd to last number.
+        // Doing this gives room for the final iteration to hold last two numbers.
         for (int i = 0; i < nums.length - 2; i++) {
+            // If the number we're at is the second at least.
+            // If the number we're at is not equal to the one before.
+            // This is so that we do not have the same triplet with the same first number.
             if (i == 0 || (i > 0 && nums[i] != nums[i - 1])) {
-                int low = i + 1;
-                int high = nums.length - 1;
-                int target = 0 - nums[i];
+                // Make the boundary to find two sum of the remaining numbers.
+                int low = i + 1; // Left boundary is one after i.
+                int high = nums.length - 1; // Right boundary is at the right end.
+                int target = 0 - nums[i]; // The sum must add to target to get triplet that sums to 0.
 
+                // Shrink our window.
                 while (low < high) {
+                    // If our two numbers equals target.
                     if (nums[low] + nums[high] == target) {
+                        // We found our numbers, so create a list and add to result list.
                         result.add(Arrays.asList(nums[i], nums[low], nums[high]));
+
+                        // These next 2 statements handles duplicate checking.
+                        // Our left boundary (second #) must not be equal to the position to the right.
                         while (low < high && nums[low] == nums[low + 1]) {
                             low++;
                         }
+                        // Our right boundary (third #) must not be equal to position to its left.
                         while (low < high && nums[high] == nums[high - 1]) {
                             high--;
                         }
+                        // Move the boundaries inward.
                         low++;
                         high--;
-                    } else if (nums[low] + nums[high] > target) {
+                    } else if (nums[low] + nums[high] > target) { // If the sum is greater than target.
+                        // Move right boundary to the left.
                         high--;
-                    } else {
+                    } else { // If the sum is less than target.
+                        // Move left boundary to the right.
                         low++;
                     }
                 }
